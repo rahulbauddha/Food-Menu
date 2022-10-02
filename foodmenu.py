e65b3f8a7b91abd19c050fc91835bf625872cc5e
rahulbauddha <rahulb22@navgurukul.org>
2022-10-02 10:57:27 +0530

    Add files via upload

---
+++ foodmenu.py
@@ -0,0 +1,33 @@
+print("welcome to food corner")
+print("1= Monday\n2= Tuesday\n3= Wednesday\n4= Thursday\n5= Friday\n6= Saturday\n7= Sunday")
+day=int(input("enter the number.. "))
+if day==1:
+    print("welcome to monday\n1= breakfast\n2= lunch\n3= dinner")
+    day_time=int(input("choose one number.. "))
+    if day_time==1:
+        print("1= poha\n2= maggie\n3= sandwich")
+        food=int(input("choose one option.. "))
+        if food==1:
+            print("1= 500g       Rs.50\n2= 1kg      Rs.100")
+            quantity=int(input("choose the quantity.."))
+            qty=50
+            if quantity==1:
+                print("Wait for 15 min\n\nTotal price is..",quantity*qty,"\n\n      THANK YOU FOR SHOOPING")
+            if quantity==2:
+                print("Wait for 15 min\n\nTotal price is..",quantity*qty,"\n\n       THANK YOU FOR SHOOPING")
+        elif food==2:
+            print("1= 500g       Rs.50\n2= 1kg      Rs.100")
+            quantity=int(input("choose the quantity.."))
+            qty=50
+            if quantity==1:
+                print("Wait for 15 min\n\nTotal price is..",quantity*qty,"\n\n      THANK YOU FOR SHOOPING")
+            if quantity==2:
+                print("Wait for 15 min\n\nTotal price is..",quantity*qty,"\n\n       THANK YOU FOR SHOOPING")
+        elif food==3:
+            print("1= 500g       Rs.50\n2= 1kg      Rs.100")
+            quantity=int(input("choose the quantity.."))
+            qty=50
+            if quantity==1:
+                print("Wait for 15 min\n\nTotal price is..",quantity*qty,"\n\n      THANK YOU FOR SHOOPING")
+            if quantity==2:
+                print("Wait for 15 min\n\nTotal price is..",quantity*qty,"\n\n       THANK YOU FOR SHOOPING")
